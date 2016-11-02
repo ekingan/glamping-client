5d7ef1c6ea63b30e39c1a95668c83825f76c7e25
ekingan <ekingan@gmail.com>
2016-11-02 12:10:04 -0700

    HOC authenticate

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
-import * as actopms from '../../actions';
+import * as actions from '../../actions';
 import InputText from '../forms/input_text';
 
 class Signup extends Component {
+
+  handleFormSubmit(formProps) {
+    console.log(formProps);
+    //call action to sign up the user
+    this.props.signupUser(formProps);
+  }
+
+  renderAlert() {
+    if (this.props.errorMessage) {
+      return (
+        <div className="alert alert-danger">
+          <strong> Ooops! </strong>
+          {this.props.errorMessage}
+        </div>
+      );
+    }
+  }
   render () {
     const { valid, invalid, handleSubmit, fields: { email, password, passwordConfirm}} = this.props;
 
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
         <InputText label='Email' type='email' {...email} />
           {email.touched && email.error && <div className="error">{email.error}</div>}
         <InputText label='Password' type='password' {...password} />
           {password.touched && passwordConfirm.touched && password.error && <div className="error">{password.error}</div>}
         <InputText label='Confirm Password' type='password' {...passwordConfirm} />
           {passwordConfirm.touched && passwordConfirm.error && <div className="error">{passwordConfirm.error}</div>}
+          {this.renderAlert()}
         <button className="btn btn-primary" action="submit" disabled={invalid}>Sign Up</button>
       </form>
     );
@@ -32,8 +50,12 @@ function validate(formProps) {
   return errors
 }
 
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.error };
+}
+
 export default reduxForm({
   form: 'signup',
   fields: ['email', 'password', 'passwordConfirm'],
   validate
-})(Signup);
+}, mapStateToProps, actions)(Signup);
